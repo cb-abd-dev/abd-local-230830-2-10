ec53152d30a2d983e812ada03a3156b932ea7e79
cb-abd-dev <cb@abd.dev>
2023-08-31 06:00:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "abd-local-230830-2-10",
+  url: "https://abd-local-230830-2-10.botics.co",
+  api: "https://abd-local-230830-2-10.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
